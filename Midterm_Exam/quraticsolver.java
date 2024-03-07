1f9180940fa4d4a9c3465f96eaff56f7801d5e84
zainab <zjassem@umich.edu>
2024-03-07 15:12:44 -0500

    c is 10

--- Midterm_Exam/quraticsolver.java
+++ Midterm_Exam/quraticsolver.java
@@ -5,7 +5,7 @@ public class quraticsolver {
 		// TODO Auto-generated method stub
 		int a = 5;
 		int b = 10;
-		int c = 15;
+		int c = 10;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
